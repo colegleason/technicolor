c43815851b350361b51bdbe249212e8008d17fe4
Cole Gleason <colewashere@gmail.com>
2013-03-26 22:51:50 -0500

    arc and frame viz working with test data

---
+++ arc.js
@@ -0,0 +1,87 @@
+function renderEpisodeArc(filename) {
+	d3.json(filename, function(jsondata) {
+		var frames = jsondata['frames'];
+		var arcs = jsondata['arcs'];
+		var frame_count = frames.length;
+		var width = 1100;
+		var height = 768;
+		var y = height * 0.8;
+		var x = d3.scale.ordinal()
+			.domain(d3.range(frame_count))
+			.rangeBands([0, width])
+
+		//sets up the basic container for the visualization
+		var chart = d3.select("#arc")
+			.append("svg")
+			.attr("class", "chart")
+			.attr("width", width)
+			.attr("height", height)
+		
+		chart.selectAll("rect")
+			.data(frames)	
+			.enter()
+			.append("rect")
+			.attr("width", width / frame_count)
+			.attr("height", 15)
+			.attr("x", function(d, i) { return x(i); } )
+			.attr("y", y)
+			.attr("fill", function(d) { return d; })
+
+		var arcGroup = chart.append("g");
+		//draw the arcs from one frame to the other
+		arcGroup.selectAll("path")
+			.data(arcs)
+			.enter().append("path")
+			.attr("d",function(d,i){
+			    //x2 is always the rightmost, so 
+			    //swap the values if neccessary
+				var point_a_x =  x(d[0]);  
+				var point_b_x = x(d[1]);
+				if(point_a_x > point_b_x){
+					x1 = point_b_x;
+					x2 = point_a_x;
+				}
+				else{
+					x1 = point_a_x;
+					x2 = point_b_x;
+				}
+				x1 = x1 + width/frame_count/2;
+				x2 = x2 + width/frame_count/2;
+				//qick calculation for the arc.  The closer the
+				//teams are to each other (on the axis), the 
+				//smaller the radii need to be
+				val = (x2 - x1)/2;
+			    return "M" + x1 + ","+y+" A "+ val +","+ val +" 0 0 1 " + x2 + ","+y
+			})
+			.attr("stroke", function(d) {return frames[d[0]];})
+			.attr("stroke-width", width / frame_count)
+			.attr("fill","none") 
+	})
+
+}
+
+//based on code from: http://mbostock.github.com/d3/ex/chord.html
+function fade(opacity, teams) {
+	return function(g, i) {
+    //fade the paths 
+		var paths = d3.select("#trades").selectAll("path").filter(function(d) {
+			return d.team_a != teams[i] && d.team_b != teams[i];
+		});
+		paths.transition().style("opacity", opacity);
+
+    //fade the images
+    //these are the trades that go with the selected team
+		var items = $.grep(d3.select("#trades").selectAll("path").data(), function(item) {
+			return $.inArray(item, paths.data()) < 0;
+		});
+    //get the team a and team b of the trade
+		var involvedTeams = $.map(items, function(value, index) {
+			return [value.team_a, value.team_b];
+		});
+    //grab all the images that aren't in the list
+    //of teams built above
+		d3.select("#trades").selectAll("image").filter(function(d) {
+			return $.inArray(d, involvedTeams) == -1;
+		}).transition().style("opacity", opacity);
+	};
+}
