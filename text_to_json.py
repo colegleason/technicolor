109a7eeb2aff589be237b7ba9ea56086da7d7550
Adam Yala <yala1@siebl-0222-16.ews.illinois.edu>
2013-03-27 22:14:10 -0500

    text_to_json script done

--- text_to_json.py
+++ text_to_json.py
@@ -1,15 +1,44 @@
 import simplejson, json
 
+from pprint import pprint
+
 colorFile = open('/home/yala1/cs467/technicolor/test_colors.txt', 'r')
 
 colorText = colorFile.readlines()
 
-arcDict = {}
+arcList = []
 
 for i in range(0, len(colorText)):
 	for j in range(0, len(colorText)):
 		if colorText[i] == colorText[j] and j <> i:
-			print i, j
+			pair = []
+			pair.append(i)
+			pair.append(j)
+			if not pair in arcList and not pair[::-1] in arcList:
+				arcList.append(pair)
+
+arcDict = {}
+
+arcDict['arcs'] = arcList
+
+nodeList = []
+
+for i in range(0, len(colorText)):
+	nodeList.append({'name': 'Episode ' + str((i+1)),'color': colorText[i]})
+
+arcDict['nodes'] = arcList
+
+#make empty list
+#make a dict with color and name
+#add values for color and name
+
+with open("test_color.json", "w") as file:
+	file.write(json.dumps(arcDict))
+			
+#testDict = {}
+#testDict['arcs'] = [0,5]
+#testDict['arcs'] = [0,7]
+#print json.dumps(testDict)
 
 #for color in colorText:
 #	print color
